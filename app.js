877583826a05516f06ce8acb6d3e7df65426de57
Shamari Lewis <superskl5000@gmail.com>
2024-03-17 13:02:42 -0700

    updated xss package

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("express-async-errors");
 // extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
+const xss = require("xss");
 const rateLimiter = require("express-rate-limit");
 
 // Swagger
